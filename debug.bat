4f51f1975829f904bce40baffc705b5a876ccb90
星齐 <Im_Luxqi@163.com>
2018-11-28 14:19:52 +0800

    last...

--- debug.bat
+++ debug.bat
@@ -2,5 +2,5 @@
 echo [INFO] Use maven jetty-plugin debug the project.
 call mvnDebug clean -Dmaven.test.skip=true  prepare-package jetty:run
 
-     cd bin
-     pause
+cd bin
+pause
--- src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-adas
-</body>
-</html>
